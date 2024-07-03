ab5821349db9c52d9fcfbd86dc416ebeb4b2bb3e
jackflaggg <rasul.khamzinnn@gmail.com>
2024-07-03 17:17:01 +0500

    ispr 3-calc-events and 3-calc

--- 3-calc-events/index.js
+++ 3-calc-events/index.js
@@ -1,27 +1,27 @@
 'use strict';
-const Calculator = require('./Calculator');
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
 const args = process.argv.slice(2);
 const num1 = parseFloat(args[1]);
 const num2 = parseFloat(args[2]);
 const operation = args[0];
 
-const calculator = new Calculator();
-
-calculator.on('+', (num1, num2) => {
+emitter.on('+', (num1, num2) => {
 	console.log(`Сумма равна : ${num1 + num2}`)
 })
 
-calculator.on('*', (num1, num2) => {
+emitter.on('*', (num1, num2) => {
 	console.log(`Умножение чисел равно : ${num1 * num2}`)
 })
 
-calculator.on('/', (num1, num2) => {
+emitter.on('/', (num1, num2) => {
 	console.log(`Деление равно : ${num1 - num2}`)
 })
 
-calculator.on('-', (num1, num2) => {
+emitter.on('-', (num1, num2) => {
 	console.log(`Вычитание равно : ${num1 - num2}`)
 })
 
 
-calculator.emit(operation, num1, num2)
+emitter.emit(operation, num1, num2)
